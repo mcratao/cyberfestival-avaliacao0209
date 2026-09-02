96e0e04ccf7764d83c602a5f57875e3b9808e25f
mcratao <118567936+mcratao@users.noreply.github.com>
2026-09-02 08:49:47 -0300

    Update Ingresso.java

--- cyberfestival-avaliacao0209/src/br/com/cyberfestival/model/Ingresso.java
+++ cyberfestival-avaliacao0209/src/br/com/cyberfestival/model/Ingresso.java
@@ -1,18 +1,18 @@
 package br.com.cyberfestival.model;
-
+// Modelo de dados que representa um ingresso do festival
 public class Ingresso {
 
-
-private String codigoId;
-private String tipo;
-private double valor;
-
+// Atributos privados do ingresso
+private String codigoId; // Identificador único (ex: "A-01")
+private String tipo;  // Categoria do ingresso (ex: "VIP", "PISTA")
+private double valor;  // Preço pago pelo ingresso
+// Construtor completo para instanciar um novo ingresso
 public Ingresso(String codigoId, String tipo, double valor) {
     this.codigoId = codigoId;
     this.tipo = tipo;
     this.valor = valor;
 }
-
+// Métodos getters para leitura dos dados protegidos
 public String getCodigoId() {
     return codigoId;
 }
