564a18854372b31bb2043d8daf3e7c2b00354680
mcratao <118567936+mcratao@users.noreply.github.com>
2026-09-02 08:50:11 -0300

    Update IngressoInvalidoException.java

--- cyberfestival-avaliacao0209/src/br/com/cyberfestival/model/IngressoInvalidoException.java
+++ cyberfestival-avaliacao0209/src/br/com/cyberfestival/model/IngressoInvalidoException.java
@@ -1,8 +1,8 @@
 package br.com.cyberfestival.model;
-
+// Exceção customizada (não-checada) para indicar fraudes ou duplicidades na bilheteria
 public class IngressoInvalidoException extends RuntimeException {
 
-
+// Construtor padrão que envia a mensagem de erro detalhada para a superclasse
 public IngressoInvalidoException() {
     super("Erro de Segurança: Ingresso já validado ou código duplicado!");
 }
